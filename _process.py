4b3431d9ed427675fd59add6a9f8e1b7128ea26a
Shivam Bansal <shivam5992@gmail.com>
2017-05-21 18:15:07 +0530

    cartesian process

--- _process.py
+++ _process.py
@@ -1,4 +1,5 @@
 from Levenshtein import ratio as lev_ratio
+import pandas as pd 
 
 class cleanup:
 	def __init__(self, data, clean_config):
@@ -30,27 +31,48 @@ class cleanup:
 
 ## control - clean and match separate 
 class matcher:
-	def levenshtein_match(text1, text2):
+	def levenshtein_match(self, text1, text2):
 		return lev_ratio(text1, text2)
 
-	def match_records(x):
-		t1 = str(x['cln_col1'])
-		t2 = str(x['cln_col2'])	
+	def match_records(self, _row, colname):
+		text1 = str(_row[colname])
+		text2 = str(_row[colname+"_"])	
 		
-		s1 = levenshtein_match(t1, t2)
-		c['confidence'] = s1
+		conf = 0
+		conf += self.levenshtein_match(text1, text2)
+		## Add other matche algos here
 
-		## Add other matche algos here 
+		return conf 
+
 
 	## Handle for multiple columns 
-	def process():
-		cartesian_pairs = pd.merge(DF1, DF2, on = [col])
+	def process(self, colname, DF1):
 
-		cartesian_pairs.apply(lambda x: match_records(x, 'halka'), axis=1)
+		# Create another dataframe with same column
+		# check for multiple columns
+		DF2 = pd.DataFrame()
+		DF2[colname+"_"] = DF1[colname]
 
-		## sort and return the id pairs 
+		# Create Cartesian Products
+		cartesian_index = '_i_n_d_e_x'
+		DF1[cartesian_index] = 0
+		DF2[cartesian_index] = 0
+		cartesian_pairs = pd.merge(DF1, DF2, on=cartesian_index)
+
+		# Matching Process
+		cartesian_pairs['confidence_score'] = cartesian_pairs.apply(lambda row: self.match_records(row, colname), axis=1)
+		cartesian_pairs = cartesian_pairs.sort_values(['confidence_score'], ascending=[False])
+
+		drop = [cartesian_index]
+		cartesian_pairs = cartesian_pairs.drop(drop, axis=1)
+
+		return cartesian_pairs
+
 
 if __name__ == '__main__':
-	matcher()
 
+	df1 = pd.read_csv('data/data1.csv')
 
+	match = matcher()
+	results = match.process('city', df1)
+	results.to_csv('test.csv', index=False)
